ffe9da4981e015e467b7c752627cc5c16cd9980c
zuer <1974900537@example.com>
2019-06-13 14:52:18 +0800

    20190613新增

--- src/二叉树/NodeMain.java
+++ src/二叉树/NodeMain.java
@@ -1,5 +1,10 @@
 package 二叉树;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class NodeMain {
 
     public static void main(String[] args) {
@@ -18,11 +23,65 @@ public class NodeMain {
         Node node=new Node("A",nodeD,nodeE);
        // System.out.println(nodeA);
 
-        preOrder(node);//中序遍历
+        //preOrder(node);//中序遍历
         System.out.println();
-        postOrder(node);//后序遍历
+        //postOrder(node);//后序遍历
         System.out.println();
-        midOrder(node);//中序遍历
+        //midOrder(node);//中序遍历
+        System.out.println();
+        System.out.println(levelOrder(node));//层序遍历
+        //System.out.println(getHight(node));//二叉树高度
+        //System.out.println(getCount(node));//节点数
+
+
+    }
+
+    private static List<List<String>> levelOrder(Node node){
+        List<List<String>> reList=new ArrayList<List<String>>();
+        Queue<Node> queue=new LinkedList<>();
+        queue.offer(node);
+        //System.out.println(queue);
+        while(!queue.isEmpty()){
+            int levelSize=queue.size();
+            List<String> subList=new ArrayList<String>();
+            while(levelSize!=0){
+                Node temp=queue.poll();//删除栈的开头，并得到开头的元素取它的值和子节点存入栈中
+                subList.add(temp.getData()+" ");
+                if(temp.getLeftChild()!=null){
+                    queue.offer(temp.getLeftChild());
+                }
+                if(temp.getRightChild()!=null){
+                    queue.offer(temp.getRightChild());
+                }
+                levelSize--;
+            }
+            reList.add(subList);
+
+        }
+        return reList;
+    }
+
+    private static int getCount(Node node){
+        if(node==null){
+
+            return 0;
+
+        }else{
+            return 1+getCount(node.getRightChild())+getCount(node.getLeftChild());
+        }
+    }
+
+    private static int getHight(Node node){
+
+        if(node==null){
+            return 0;
+        }else{
+            int i=getHight(node.getLeftChild());
+            int j=getHight(node.getRightChild());
+            return i>=j?i+1:j+1;
+        }
+
+
     }
 
 
