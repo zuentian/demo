9ff9acf97fa6e791a927a6168f7b8545593ec6b9
zuer <1974900537@example.com>
2019-06-13 13:47:48 +0800

    20190613新增

--- src/二叉树/NodeMain.java
+++ src/二叉树/NodeMain.java
@@ -15,10 +15,47 @@ public class NodeMain {
         Node nodeM=new Node("M",null,null);
         Node nodeD=new Node("D",nodeF,nodeM);
         Node nodeE=new Node("E",nodeN,null);
-        Node nodeA=new Node("A",nodeD,nodeE);
-        System.out.println(nodeA);
+        Node node=new Node("A",nodeD,nodeE);
+       // System.out.println(nodeA);
+
+        preOrder(node);//中序遍历
+        System.out.println();
+        postOrder(node);//后序遍历
+        System.out.println();
+        midOrder(node);//中序遍历
     }
 
 
+    private static void preOrder(Node node){
+        if(node ==null){
+            return;
+        }else{
+            System.out.print(node.getData()+" ");
+            preOrder(node.getLeftChild());
+            preOrder(node.getRightChild());
+        }
+
+    }
+
+    private static void postOrder(Node node){
+        if(node==null){
+            return;
+        }else {
+            postOrder(node.getLeftChild());
+            postOrder(node.getRightChild());
+            System.out.print(node.getData()+" ");
+        }
+
+    }
+
+    private static void midOrder(Node node){
+        if(node==null){
+            return;
+        }else {
+            midOrder(node.getLeftChild());
+            System.out.print(node.getData()+" ");
+            midOrder(node.getRightChild());
+        }
+    }
 
 }
